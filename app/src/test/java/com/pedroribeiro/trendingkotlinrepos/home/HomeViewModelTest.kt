40128a3689e01e67ba510c8b6f7d9fb15652d25b
ribas1 <ribasss@gmail.com>
2020-09-13 15:46:00 +0100

    Add home viewmodel tests

---
+++ app/src/test/java/com/pedroribeiro/trendingkotlinrepos/home/HomeViewModelTest.kt
@@ -0,0 +1,83 @@
+package com.pedroribeiro.trendingkotlinrepos.home
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import com.pedroribeiro.domain.TrendingRepoRepository
+import com.pedroribeiro.domain.models.RepositoryDomainModel
+import com.pedroribeiro.trendingkotlinrepos.home.utils.LifecycleOwnerUtils
+import com.pedroribeiro.trendingkotlinrepos.mappers.RepositoryModelMapper
+import com.pedroribeiro.trendingkotlinrepos.models.RepositoryUiModel
+import com.pedroribeiro.trendingkotlinrepos.schedulers.TrampolineSchedulerProviderImpl
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.rxjava3.core.Single
+import org.junit.Before
+import org.junit.Test
+import org.junit.Rule
+
+class HomeViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: HomeViewModel
+    private val repoRepository = mockk<TrendingRepoRepository>(relaxed = true)
+    private val repoModelMapper = mockk<RepositoryModelMapper>(relaxed = true)
+    private val lifecycleOwner = mockk<LifecycleOwner>(relaxed = true)
+
+    @Before
+    fun setup() {
+        viewModel = HomeViewModel(
+            repoRepository,
+            repoModelMapper,
+            TrampolineSchedulerProviderImpl()
+        )
+        LifecycleOwnerUtils.setupLifecycleOwner(lifecycleOwner)
+    }
+
+    @Test
+    fun `GIVEN user opened app WHEN home fragment is attached THEN get trending kotlin repositories successfully`() {
+        val observer = mockk<Observer<List<RepositoryUiModel>>>(relaxed = true)
+        val mockedTrendingRepoDomainModel =
+            mockk<List<RepositoryDomainModel>>(relaxed = true)
+        viewModel.repositories.observe(lifecycleOwner, observer)
+        every { repoRepository.getTrendingRepositories() } returns Single.just(
+            mockedTrendingRepoDomainModel
+        )
+        val uiModel = repoModelMapper.mapToUi(mockedTrendingRepoDomainModel)
+
+        viewModel.getTrendingRepos()
+
+        verify {
+            observer.onChanged(uiModel)
+        }
+    }
+
+    @Test
+    fun `GIVEN user opened app WHEN home fragment is attached THEN get trending kotlin repositories with an error`() {
+        val observer = mockk<Observer<Unit>>(relaxed = true)
+        viewModel.error.observe(lifecycleOwner, observer)
+        every { repoRepository.getTrendingRepositories() } returns Single.error(Throwable())
+
+        viewModel.getTrendingRepos()
+
+        verify {
+            observer.onChanged(Unit)
+        }
+    }
+
+    @Test
+    fun `GIVEN user sees a list of repositories WHEN user clicks on of the repositories THEN navigate to repository details`() {
+        val observer = mockk<Observer<HomeViewModel.Navigation>>(relaxed = true)
+        val mockedRepo = mockk<RepositoryUiModel>(relaxed = true)
+        viewModel.navigation.observe(lifecycleOwner, observer)
+
+        viewModel.onRepositoryClick(mockedRepo)
+
+        verify {
+            observer.onChanged(HomeViewModel.Navigation.ToRepoDetails(mockedRepo))
+        }
+    }
+}
---
+++ app/src/test/java/com/pedroribeiro/trendingkotlinrepos/home/utils/LifecycleOwnerUtils.kt
@@ -0,0 +1,18 @@
+package com.pedroribeiro.trendingkotlinrepos.home.utils
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import io.mockk.every
+
+object LifecycleOwnerUtils {
+    /**
+     * Setup the lifecycle owner that defines a mock lifecycle return to prevent LiveData crash on
+     * observe function (LiveData.java : 165)
+     */
+    fun setupLifecycleOwner(lifecycleOwner: LifecycleOwner) {
+        val lifecycle = LifecycleRegistry(lifecycleOwner)
+        every { lifecycleOwner.lifecycle } returns lifecycle
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+}
